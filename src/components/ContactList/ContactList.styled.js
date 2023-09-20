99fbeb966a48b8d6354e4e021d86f9125b226b3c
pnyekrasov <pnekrasov1910@gmail.com>
2023-09-21 01:32:09 +0300

    start

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonReset = styled.button`
+  margin-bottom: 12px;
+`;
+
+export const List = styled.ul`
+  margin-top: 12px;
+  margin-left: 44px;
+`;
+
+export const ListItem = styled.li`
+  margin-bottom: 12px;
+`;
