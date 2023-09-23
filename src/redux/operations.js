9a78d9a0131ebffc7adb75bcf00e2351d60309d0
pnyekrasov <pnekrasov1910@gmail.com>
2023-09-23 13:57:12 +0300

    HW 6

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
+axios.defaults.baseURL = 'https://650b13b2dfd73d1fab098931.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = state => state.contacts;
+export const selectContacts = state => state.contacts.items;
 
 export const selectIsLoading = state => state.contacts.isLoading;
 
